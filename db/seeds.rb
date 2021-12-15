12e1c3da1112af7daef31f4fbee38b5e456ff8dd
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2021-12-15 10:01:11 +0100

    Seed create

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Doctor.create(first_name: 'Jean', last_name: 'Dupont', speciality: 'Généraliste')
+Doctor.create(first_name: 'Michel', last_name: 'Bois', speciality: 'Dentiste')
+Doctor.create(first_name: 'Arthur', last_name: 'Sartre', speciality: 'Podologue')
+
+Patient.create(first_name: 'Hugo', last_name: 'Moulinet', social_security_number: '0000')
+Patient.create(first_name: 'Julien', last_name: 'Martin', social_security_number: '1111')
+Patient.create(first_name: 'Robert', last_name: 'Petit', social_security_number: '2222')
