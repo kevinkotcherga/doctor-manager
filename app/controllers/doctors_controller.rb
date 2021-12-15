d648cd8b9be989fce3331e742c041da449a077ef
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2021-12-15 12:32:27 +0100

    Create Doctor

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,20 @@
 class DoctorsController < ApplicationController
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      redirect_to consultations_path(@consultation)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:first_name, :last_name, :speciality)
+  end
 end
